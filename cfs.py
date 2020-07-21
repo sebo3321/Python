4dea6d0a1b042640331b8c7c3be46cc72fd686d2
sebo3321 <68596263+sebo3321@users.noreply.github.com>
2020-07-21 13:29:08 +0100

    Add files via upload
    Program that allow you to create folders and files with static name and automatically generated number attached to it.

---
+++ cfs.py
@@ -0,0 +1,16 @@
+# Create folders and files with static name
+# and automatically generated number attached to it
+import os
+
+def createFolder(directory):
+    if not os.path.exists(directory):
+        os.mkdir(directory)
+
+n = 1
+while n <= 100: #<- Change this value if you want less or more folders
+    print(n)
+    # Change on your directory and name of files you want
+    createFolder('C:/Users/sebci/Desktop/Python exercise/cfs/ex' + str(n) + '/')
+    f = open('C:/Users/sebci/Desktop/Python exercise/cfs/ex' + str(n) + '/ex' + str(n) + '.py', 'w+')
+    int(n)
+    n = n + 1
