526d8685d46d1750b01bd0bcad1c45727ecae9ca
sebo3321 <68596263+sebo3321@users.noreply.github.com>
2022-10-05 21:10:54 +0100

    Create Letter frequency.py

---
+++ Letter frequency.py
@@ -0,0 +1,108 @@
+count = 0
+a = 0
+b = 0
+c = 0
+d = 0
+e = 0
+f = 0
+g = 0
+h = 0
+i = 0
+j = 0
+k = 0
+l = 0
+m = 0
+n = 0
+o = 0
+p = 0
+q = 0
+r = 0
+s = 0
+t = 0
+u = 0
+v = 0
+w = 0
+x = 0
+y = 0
+z = 0
+string = "You can have data without information, but you cannot have information without data." 
+string = string.lower()
+for el in range(0, len(string)):
+  if "a" == string[el]:
+    a = a + 1
+  elif "b" == string[el]:
+    b = b + 1
+  elif "c" == string[el]:
+    c = c + 1
+  elif "d" == string[el]:
+    d = d + 1
+  elif "e" == string[el]:
+    e = e + 1
+  elif "f" == string[el]:
+    f = f + 1
+  elif "g" == string[el]:
+    g = g + 1
+  elif "h" == string[el]:
+    h = h + 1
+  elif "i" == string[el]:
+    i = i + 1
+  elif "j" == string[el]:
+    j = j + 1
+  elif "k" == string[el]:
+    k = k + 1
+  elif "l" == string[el]:
+    l = l + 1
+  elif "m" == string[el]:
+    m = m + 1
+  elif "n" == string[el]:
+    n = n + 1
+  elif "o" == string[el]:
+    o = o + 1
+  elif "p" == string[el]:
+    p = p + 1
+  elif "q" == string[el]:
+    q = q + 1
+  elif "r" == string[el]:
+    r = r + 1
+  elif "s" == string[el]:
+    s = s + 1
+  elif "t" == string[el]:
+    t = t + 1
+  elif "u" == string[el]:
+    u = u + 1
+  elif "v" == string[el]:
+    v = v + 1
+  elif "w" == string[el]:
+    w = w + 1
+  elif "x" == string[el]:
+    x = x + 1
+  elif "y" == string[el]:
+    y = y + 1
+  elif "z" == string[el]:
+    z = z + 1
+print("a : " + str(a))
+print("b : " + str(b))
+print("c : " + str(c))
+print("d : " + str(d))
+print("e : " + str(e))
+print("f : " + str(f))
+print("g : " + str(g))
+print("h : " + str(h))
+print("i : " + str(i))
+print("j : " + str(j))
+print("k : " + str(k))
+print("l : " + str(l))
+print("m : " + str(m))
+print("n : " + str(n))
+print("o : " + str(o))
+print("p : " + str(p))
+print("q : " + str(q))
+print("r : " + str(r))
+print("s : " + str(s))
+print("t : " + str(t))
+print("u : " + str(u))
+print("v : " + str(v))
+print("w : " + str(w))
+print("x : " + str(x))
+print("y : " + str(y))
+print("z : " + str(z))
